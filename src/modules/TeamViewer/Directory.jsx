ea0661807edc415909463c4287928b12f73adcc5
ministrydad <goggin.philip@gmail.com>
2025-11-07 21:03:57 -0600

    udpates

--- src/modules/TeamViewer/Directory.jsx
+++ src/modules/TeamViewer/Directory.jsx
@@ -2434,56 +2434,6 @@ function ProfessorRolesCard({ profile, isEditMode, onFieldChange }) {
             transform: translateX(0);
           }
         }
-        
-        /* iOS-style toggle switches */
-        .switch {
-          position: relative;
-          display: inline-block;
-          width: 44px;
-          height: 24px;
-        }
-
-        .switch input {
-          opacity: 0;
-          width: 0;
-          height: 0;
-        }
-
-        .slider {
-          position: absolute;
-          cursor: pointer;
-          top: 0;
-          left: 0;
-          right: 0;
-          bottom: 0;
-          background-color: #ccc;
-          transition: .4s;
-          border-radius: 24px;
-        }
-
-        .slider:before {
-          position: absolute;
-          content: "";
-          height: 18px;
-          width: 18px;
-          left: 3px;
-          bottom: 3px;
-          background-color: white;
-          transition: .4s;
-          border-radius: 50%;
-        }
-
-        input:checked + .slider {
-          background-color: #2ea44f;
-        }
-
-        input:focus + .slider {
-          box-shadow: 0 0 1px #2ea44f;
-        }
-
-        input:checked + .slider:before {
-          transform: translateX(20px);
-        }
       `}</style>
     </>
   );
